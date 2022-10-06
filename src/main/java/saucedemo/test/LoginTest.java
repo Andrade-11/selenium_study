55ceea7d8d549d0b0cd0977716f7b1e64eb37faa
Gabriel Andrade <andrade.prog.11@gmail.com>
2022-10-06 16:13:08 -0300

    Improve quality in Readme and add one more method in LoginTest

--- src/main/java/saucedemo/test/LoginTest.java
+++ src/main/java/saucedemo/test/LoginTest.java
@@ -7,11 +7,18 @@ import saucedemo.pages.LoginPage;
 
 
 public class LoginTest extends BaseTest {
-
+    private LoginPage user;
+    public LoginTest(){
+        user = new LoginPage();
+    }
     @Test
     public void sucessfullLogin() throws InterruptedException{
-        LoginPage loginPage = new LoginPage();
-        loginPage.login("standard_user", "secret_sauce");
-        Assert.assertEquals(loginPage.searchWordInInventory(), "PRODUCTS");
+        user.login("standard_user", "secret_sauce");
+        Assert.assertEquals(user.searchWordInInventory(), "PRODUCTS");
+    }
+    @Test
+    public void messageErrorLogin() throws InterruptedException{
+    user.login("standard_user","21321321");
+    Assert.assertEquals(user.searchMensageErrorLogin(),"Epic sadface: Username and password do not match any user in this service");
     }
 }
