7573c4c6695141419942dcf00ec34ad082263202
Gabriel Andrade <andrade.prog.11@gmail.com>
2023-01-17 13:48:09 -0300

    .

---
+++ src/main/java/saucedemo/pages/OptionsPage.java
@@ -0,0 +1,26 @@
+package saucedemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import static saucedemo.driver.DriverFactory.getDefaultWait;
+import static saucedemo.driver.DriverFactory.getDriver;
+
+public class OptionsPage extends BasePage{
+    By options = By.id("react-burger-menu-btn");
+    By about = By.id("about_sidebar_link");
+    By logout = By.id("logout_sidebar_link");
+    By superTitle = By.xpath("//*[@id=\"entry-3qDFahnypj1KkiORyU1Zyh\"]/div/div/div/div[1]/div/h1");
+    public void aboutSauceDemo(){
+        getDefaultWait().until(ExpectedConditions.elementToBeClickable(options)).click();
+        reusable.clickInButton(about);
+    }
+    public void logoutSauceDemo(){
+        reusable.clickInButton(options);
+        reusable.clickInButton(logout);
+    }
+
+    public String getTextSuperTitle(){
+        return getDriver().findElement(superTitle).getText();
+    }
+}
---
+++ src/main/java/saucedemo/test/OptionsTest.java
@@ -0,0 +1,13 @@
+package saucedemo.test;
+
+import saucedemo.database.interfaces.GetCredencial;
+//Will Be Implemented soon.
+public class OptionsTest extends BaseTest implements GetCredencial {
+
+    //@Test
+    //public void aboutSauceDemo(){
+       // user.login(getUsernameValue(0), getPasswordValue(0));
+     //   options.aboutSauceDemo();
+      //  Assert.assertEquals(options.getTextSuperTitle(),"DEVELOP WITH CONFIDENCE");
+    //}
+}
