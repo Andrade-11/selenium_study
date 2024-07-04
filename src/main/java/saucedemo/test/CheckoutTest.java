4e383e17784632f0d39af3a47e3c8ee87effe4e0
Gabriel Andrade <andrade.prog.11@gmail.com>
2024-07-04 20:04:09 -0300

    a

--- src/main/java/saucedemo/test/CheckoutTest.java
+++ src/main/java/saucedemo/test/CheckoutTest.java
@@ -3,12 +3,13 @@ package saucedemo.test;
 
 import org.junit.Assert;
 import org.junit.Test;
+import saucedemo.database.interfaces.GetCredencial;
 
 
-public class CheckoutTest extends BaseTest {
+public class CheckoutTest extends BaseTest implements GetCredencial {
         @Test
         public void sucessfullOrder() {
-                user.login("standard_user","secret_sauce");
+                user.login(getUsernameValue(0),getPasswordValue(0));
                 inventory.getItemInTheCart();
                 checkout.submitForm("Gabriel Augusto","Dias de Andrade","2312321321");
                 Assert.assertEquals(checkout.searchThankYouOrder(),"THANK YOU FOR YOUR ORDER");
